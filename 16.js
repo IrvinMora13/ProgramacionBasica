55941fea0b0bdf622f6c930a56c30a3c08987cae
Irvin Morales <a343423@gmail.com>
2023-11-02 14:29:28 -0600

    Problema16
    16.- Imprimir diez veces la serie de números del 1 al 10.

---
+++ 16.js
@@ -0,0 +1,12 @@
+//16.- Imprimir diez veces la serie de números del 1 al 10. 
+function repit10() {
+    for (let i = 1; i <= 10; i++) {
+        console.log("Ciclo ",i," :");
+        for (let j = 1; j <= 10; j++) {
+          console.log(j);
+        }
+      }
+      
+}
+
+repit10();
