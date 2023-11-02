9546e120d4ff64b6535adcf69c6a892a33cb3bcc
Irvin Morales <a343423@gmail.com>
2023-11-02 10:31:11 -0600

    Problema3
    3.- Hacer un programa que imprima los números pares entre 0 y 100.

---
+++ 3.js
@@ -0,0 +1,11 @@
+//3.- Hacer un programa que imprima los números pares entre 0 y 100. 
+function numerosPares() {
+    let numero = 0;
+    while (numero <= 100 && numero >=0) {
+      console.log(numero);
+      numero += 2;
+    }
+  }
+  
+  numerosPares();
+  
