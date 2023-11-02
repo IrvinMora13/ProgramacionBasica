d9d9ffc260ec5f4b4aa55c4f6f22cdce887acbec
Irvin Morales <a343423@gmail.com>
2023-11-02 10:26:08 -0600

    Problema1
    1.- Hacer un programa que imprima los números del 1 al 100.

---
+++ 1.js
@@ -0,0 +1,12 @@
+//1.- Hacer un programa que imprima los números del 1 al 100. 
+function print100() {
+    let ini = 1
+    while (ini <= 100) {
+        if (ini >=0 && ini <= 100) {
+            console.log(ini);
+        }
+        ini += 1
+    }
+}
+
+print100()
