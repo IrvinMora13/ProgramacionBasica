d463d6f0bc58c0985d50677800b2e7ac5bb6fdc8
Irvin Morales <a343423@gmail.com>
2023-11-02 18:45:36 -0600

    problema32
    32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista
    debiendo ir cada uno en una fila distinta

---
+++ 32.js
@@ -0,0 +1,9 @@
+/*32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista
+debiendo ir cada uno en una fila distinta. */
+const nombres = ["Juan","María","Luis","Ana","Carlos","Laura","Pedro","Sofía","Diego","Elena","Miguel","Isabel","Andrés","Carmen","Javier","Rosa","Ricardo","Lourdes","Gabriel"
+  ];
+  
+  for (let i = 0; i < nombres.length; i++) {
+    console.log(nombres[i]);
+  }
+  
