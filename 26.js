99a796ccd1e28ad67c7d3448b7840fd2624a4ba8
Irvin Morales <a343423@gmail.com>
2023-11-02 23:14:58 -0600

    Problema26
    26.- Introducir una frase por teclado e imprimirla en el centro de la pantalla.

--- 26.js
+++ 26.js
@@ -1,2 +1,15 @@
 //26.- Introducir una frase por teclado e imprimirla en el centro de la pantalla. 
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Introduce una frase: ', (frase) => {
+  const anchoPantalla = process.stdout.columns;
+  const espaciosEnBlanco = Math.floor((anchoPantalla - frase.length) / 2);
+  const fraseCentrada = ' '.repeat(espaciosEnBlanco) + frase;
+  console.log(fraseCentrada);
+  rl.close();
+});
 
