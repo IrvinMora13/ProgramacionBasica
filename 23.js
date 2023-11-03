d45b2c92037225bbf5defe0ce491a3ac2cb3e78f
Irvin Morales <a343423@gmail.com>
2023-11-02 22:10:23 -0600

    Problema23
    23.- Hacer un programa que imprima los números del 0 al 100, controlando las filas y las
    columnas

--- 23.js
+++ 23.js
@@ -1,2 +1,13 @@
 /*23.- Hacer un programa que imprima los números del 0 al 100, controlando las filas y las
 columnas */
+for (let fila = 0; fila <= 10; fila++) {
+    let filaNumeros = '';
+    for (let columna = 0; columna <= 10; columna++) {
+      const numero = fila * 11 + columna;
+      if (numero <= 100) {
+        filaNumeros += numero.toString().padStart(4) + ' ';
+      }
+    }
+    console.log(filaNumeros);
+  }
+  
