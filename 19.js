195c3d9b41411cc38dd87cb751bec3e3ec51dce3
Irvin Morales <a343423@gmail.com>
2023-11-02 22:03:21 -0600

    Problema19
    19.- Hacer un programa que simule el funcionamiento de un reloj digital y que permita ponerlo en
    hora*

--- 19.js
+++ 19.js
@@ -1,3 +1,19 @@
 /*19.- Hacer un programa que simule el funcionamiento de un reloj digital y que permita ponerlo en
 hora*/
+function relojDigital() {
+    const horaActual = new Date();
+    const horas = horaActual.getHours();
+    const minutos = horaActual.getMinutes();
+    const segundos = horaActual.getSeconds();
+    console.clear(); 
+    console.log(`Reloj Digital: ${agregarCeros(horas)}:${agregarCeros(minutos)}:${agregarCeros(segundos)}`);
+  }
 
+  function agregarCeros(valor) {
+    if (valor < 10) {
+      return "0" + valor;
+    }
+    return valor;
+  }
+  
+  setInterval(relojDigital, 1000);
